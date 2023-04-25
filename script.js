58116d4618acd2bb80dc1149e4120cb7447d3049
Sarayeo <yeokpeusseusarafiela@gmail.com>
2023-04-25 15:13:49 +0200

    :construction::lipstick:adding style and content

---
+++ script.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        //   =======scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    // s===========lide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Félin", " Solitaire", "Discret"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
+
+$(document).ready(function(){
+    $(".testimonial .indicators li").click(function(){
+      var i = $(this).index();
+      var targetElement = $(".testimonial .tabs li");
+      targetElement.eq(i).addClass('active');
+      targetElement.not(targetElement[i]).removeClass('active');
+              });
+              $(".testimonial .tabs li").click(function(){
+                  var targetElement = $(".testimonial .tabs li");
+                  targetElement.addClass('active');
+                  targetElement.not($(this)).removeClass('active');
+              });
+          });
+  $(document).ready(function(){
+      $(".slider .swiper-pagination span").each(function(i){
+          $(this).text(i+1).prepend("0");
+      });
+  });
+
+  
